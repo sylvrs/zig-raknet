c19dfbbf61d9a2300b9c295173c45d54309847ff
sylvrs <sylvrs@proton.me>
2023-08-28 20:02:26 -0500

    feat!(Server): implement logger & move `init` fields to single `options` struct

--- src/raknet.zig
+++ src/raknet.zig
@@ -1,7 +1,10 @@
 const std = @import("std");
 const network = @import("network");
-const message = @import("message.zig");
+const message = @import("message/message.zig");
+const DataMessage = message.DataMessage;
+const UnconnectedMessage = message.UnconnectedMessage;
 const Connection = @import("Connection.zig");
+const Logger = @import("utils/Logger.zig");
 
 /// The magic bytes used to identify an offline message in RakNet
 pub const RakNetMagic: []const u8 = &.{ 0x00, 0xff, 0xff, 0x00, 0xfe, 0xfe, 0xfe, 0xfe, 0xfd, 0xfd, 0xfd, 0xfd, 0x12, 0x34, 0x56, 0x78 };
@@ -18,42 +21,26 @@ pub const Server = struct {
     address: network.EndPoint,
     connections: std.AutoHashMap(network.EndPoint, Connection),
     allocator: std.mem.Allocator,
+    logger: Logger,
     running: bool = true,
-    verbose: bool = false,
     socket: network.Socket = undefined,
 
-    pub fn init(allocator: std.mem.Allocator, name: []const u8, guid: i64, address: network.EndPoint) !Server {
+    /// Initializes a new Server from the given options
+    pub fn init(options: struct {
+        allocator: std.mem.Allocator,
+        name: []const u8,
+        guid: i64,
+        address: network.EndPoint,
+        verbose: bool = false,
+    }) Server {
         return .{
-            .name = name,
-            .guid = guid,
-            .address = address,
-            .connections = std.AutoHashMap(network.EndPoint, Connection).init(allocator),
-            .allocator = allocator,
+            .name = options.name,
+            .guid = options.guid,
+            .address = options.address,
+            .connections = std.AutoHashMap(network.EndPoint, Connection).init(options.allocator),
+            .allocator = options.allocator,
+            .logger = .{ .verbose = options.verbose },
         };
-    }
-
-    pub fn logErr(self: *Server, comptime format: []const u8, args: anytype) void {
-        if (self.verbose) {
-            std.log.err(format, args);
-        }
-    }
-
-    pub fn logInfo(self: *Server, comptime format: []const u8, args: anytype) void {
-        if (self.verbose) {
-            std.log.info(format, args);
-        }
-    }
-
-    pub fn logWarn(self: *Server, comptime format: []const u8, args: anytype) void {
-        if (self.verbose) {
-            std.log.warn(format, args);
-        }
-    }
-
-    pub fn logDebug(self: *Server, comptime format: []const u8, args: anytype) void {
-        if (self.verbose) {
-            std.log.debug(format, args);
-        }
     }
 
     /// Start server and listen for incoming connections
@@ -66,10 +53,10 @@ pub const Server = struct {
         // start listening
         try self.socket.bind(self.address);
         // start accepting connections
-        self.logInfo("Listening on {any}", .{self.address});
+        self.logger.info("Listening on {any}", .{self.address});
         while (self.running) {
             self.receive() catch |err| {
-                self.logErr("Error while receiving packet: {any}", .{err});
+                self.logger.err("Error while receiving packet: {any}", .{err});
             };
         }
     }
@@ -82,15 +69,15 @@ pub const Server = struct {
         var associated_connection = self.connections.get(details.sender);
         // if we don't have a connection, attempt to handle it as an offline message
         if (associated_connection) |*found_connection| {
-            const msg = message.DataMessage.from(self.allocator, raw) catch |err| {
-                self.logErr("Error while decoding datagram message (0x{x:0>2}) from {s}: {any}", .{ raw[0], details.sender, err });
+            const msg = DataMessage.from(self.allocator, raw) catch |err| {
+                self.logger.err("Error while decoding datagram message (0x{x:0>2}) from {s}: {any}", .{ raw[0], details.sender, err });
                 return;
             };
             try found_connection.handleMessage(msg);
         } else {
             // attempt to decode the message (or skip it if it's invalid)
-            const msg = message.UnconnectedMessage.from(raw) catch |err| {
-                self.logErr("Error while decoding offline message (0x{x:0>2}) from {s}: {any}", .{ raw[0], details.sender, err });
+            const msg = UnconnectedMessage.from(raw) catch |err| {
+                self.logger.err("Error while decoding offline message (0x{x:0>2}) from {s}: {any}", .{ raw[0], details.sender, err });
                 return;
             };
             try self.handleUnconnectedMessage(details.sender, msg);
@@ -98,35 +85,35 @@ pub const Server = struct {
     }
 
     /// Handles a decoded, unconnected message
-    fn handleUnconnectedMessage(self: *Server, sender: network.EndPoint, received_message: message.UnconnectedMessage) !void {
+    fn handleUnconnectedMessage(self: *Server, sender: network.EndPoint, received_message: UnconnectedMessage) !void {
         switch (received_message) {
-            .UnconnectedPing => {
+            .UnconnectedPing => |msg| {
                 // send pong back
                 try self.sendUnconnectedMessage(
                     sender,
-                    message.UnconnectedMessage.createUnconnectedPong(
-                        received_message.UnconnectedPing.ping_time,
+                    UnconnectedMessage.createUnconnectedPong(
+                        msg.ping_time,
                         self.guid,
                         self.name,
                     ),
                 );
             },
             .OpenConnectionRequest1 => {
-                self.logInfo("Received OpenConnectionRequest1 from {s}: {any}", .{ sender, received_message });
+                self.logger.info("Received OpenConnectionRequest1 from {s}: {any}", .{ sender, received_message });
                 try self.sendUnconnectedMessage(
                     sender,
-                    message.UnconnectedMessage.createOpenConnectionReply1(
+                    UnconnectedMessage.createOpenConnectionReply1(
                         self.guid,
                         false,
                         @intCast(received_message.OpenConnectionRequest1.mtu_padding.len),
                     ),
                 );
             },
-            .OpenConnectionRequest2 => {
-                self.logInfo("Received OpenConnectionRequest2 from {s}: {any}", .{ sender, received_message });
+            .OpenConnectionRequest2 => |msg| {
+                self.logger.info("Received OpenConnectionRequest2 from {s}: {any}", .{ sender, received_message });
                 try self.sendUnconnectedMessage(
                     sender,
-                    message.UnconnectedMessage.createOpenConnectionReply2(
+                    UnconnectedMessage.createOpenConnectionReply2(
                         self.guid,
                         sender,
                         received_message.OpenConnectionRequest2.mtu_size,
@@ -137,17 +124,49 @@ pub const Server = struct {
                 var new_connection = Connection{
                     .address = sender,
                     .server = self,
-                    .mtu_size = received_message.OpenConnectionRequest2.mtu_size,
-                    .client_guid = received_message.OpenConnectionRequest2.client_guid,
+                    .mtu_size = msg.mtu_size,
+                    .client_guid = msg.client_guid,
                 };
                 try self.connections.put(sender, new_connection);
-                self.logInfo("Created new connection for {s}", .{sender});
+                self.logger.info("Created new connection for {s}", .{sender});
             },
-            else => self.logWarn("Received unknown packet from {s}", .{sender}),
+            else => self.logger.warn("Received unknown packet from {s}", .{sender}),
         }
     }
 
-    pub fn sendUnconnectedMessage(self: *Server, receiver: network.EndPoint, msg: message.UnconnectedMessage) !void {
+    /// Sends an unconnected message to the specified receiver
+    pub fn sendUnconnectedMessage(self: *Server, receiver: network.EndPoint, msg: UnconnectedMessage) !void {
+        var write_buffer = [_]u8{0} ** MaxMTUSize;
+        var stream = std.io.fixedBufferStream(&write_buffer);
+        var writer = stream.writer();
+        try msg.encode(writer);
+        _ = try self.socket.sendTo(receiver, stream.getWritten());
+    }
+};
+
+pub const Client = struct {
+    allocator: std.mem.Allocator,
+    logger: Logger,
+    connected: bool = false,
+    verbose: bool = false,
+    socket: network.Socket = undefined,
+
+    /// Initializes a new Client from the given options
+    pub fn init(options: struct { allocator: std.mem.Allocator, verbose: bool = false }) Client {
+        return .{
+            .allocator = options.allocator,
+            .logger = .{ .verbose = options.verbose },
+        };
+    }
+
+    /// Start client and listen for incoming connections
+    pub fn connect(self: *Client, address: network.EndPoint) !void {
+        _ = address;
+        _ = self;
+    }
+
+    /// Sends an unconnected message to the specified receiver
+    pub fn sendUnconnectedMessage(self: *Server, receiver: network.EndPoint, msg: UnconnectedMessage) !void {
         var write_buffer = [_]u8{0} ** MaxMTUSize;
         var stream = std.io.fixedBufferStream(&write_buffer);
         var writer = stream.writer();
