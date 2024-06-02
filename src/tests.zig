387440a5520910049b9ba47a23c9532a85b4230e
sylvrs <sylvrs@proton.me>
2024-06-02 01:57:29 -0500

    chore(tests): add comment about the root test

--- src/tests.zig
+++ src/tests.zig
@@ -6,6 +6,8 @@ pub const frame = @import("message/frame.zig");
 pub const unconnected_ping = @import("message/unconnected.zig");
 pub const raknet = @import("raknet.zig");
 
+// `refAllDecls(@This())` will search for any tests in the declarations (e.g., the `@import`s above) and attempt to run them.
+// The declarations must be marked as `pub` for the function to be able to access them.
 test {
     std.testing.refAllDecls(@This());
 }
